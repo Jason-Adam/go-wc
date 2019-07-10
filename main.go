a4b5937a88301da2c8f1b932723eced915d966a6
Jason Adam <jasonadam@Jasons-MacBook-Pro.local>
2019-07-10 13:47:00 -0700

    added -w flag to main.go

--- main.go
+++ main.go
@@ -13,27 +13,60 @@ func countWordsInFile(s string) int {
 	return len(words)
 }
 
+func countLinesInFile(s int) int {
+	return s
+}
+
+func countCharsInFile(s int) int {
+	return s
+}
+
 func errorCheck(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+func retrieveCounts(d string) []int {
+	s := make([]int, 3)
+	s[0] = countWordsInFile(d)
+	s[1] = countLinesInFile(10)
+	s[2] = countCharsInFile(100)
+	return s
+}
+
 func main() {
 
+	words := flag.Bool("w", false, "")
+	//lines := flag.Bool("l", false, "")
+	//characters := flag.Bool("c", false, "")
 	flag.Parse()
 
 	switch flag.NArg() {
 	case 0:
 		data, err := ioutil.ReadAll(os.Stdin)
 		errorCheck(err)
-		wc := countWordsInFile(string(data))
-		fmt.Println(wc)
+
+		fileCounts := retrieveCounts(string(data))
+
+		if *words {
+			fmt.Println(fileCounts[0])
+		} else {
+			fmt.Println(fileCounts)
+		}
+
 	case 1:
 		data, err := ioutil.ReadFile(flag.Arg(0))
 		errorCheck(err)
-		wc := countWordsInFile(string(data))
-		fmt.Println(wc)
+
+		fileCounts := retrieveCounts(string(data))
+
+		if *words {
+			fmt.Println(fileCounts[0], flag.Arg(0))
+		} else {
+			fmt.Println(fileCounts, flag.Arg(0))
+		}
+
 	default:
 		fmt.Println("No input given")
 		os.Exit(1)
