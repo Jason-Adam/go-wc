321e2c3a76644b3967cb6deb5bd59b753988366e
Jason Adam <jasonadam@Jasons-MacBook-Pro.local>
2019-07-12 10:27:44 -0700

    updated function names and added function commenting

--- main.go
+++ main.go
@@ -16,28 +16,25 @@ type flags struct {
 	chars bool
 }
 
+// wcString prints to stdout based on flags.
 func (f *flags) wcString(s string) string {
 	var printValues string
-
 	if f.words {
 		printValues += (wordsInFile(s) + " ")
 	}
-
 	if f.lines {
 		printValues += (linesInFile(s) + " ")
 	}
-
 	if f.chars {
 		printValues += (charsInFile(s) + " ")
 	}
-
 	if !f.words && !f.lines && !f.chars {
 		return wordsInFile(s) + " " + linesInFile(s) + " " + charsInFile(s)
 	}
-
 	return printValues
 }
 
+// wordsInFile counts total words in the file.
 func wordsInFile(s string) string {
 	words := strings.Fields(s)
 	l := len(words)
@@ -45,6 +42,7 @@ func wordsInFile(s string) string {
 	return o
 }
 
+// linesInFile counts the total lines in the file.
 func linesInFile(s string) string {
 	scanner := bufio.NewScanner(strings.NewReader(s))
 	scanner.Split(bufio.ScanLines)
@@ -56,39 +54,34 @@ func linesInFile(s string) string {
 	return o
 }
 
+// charsInFile returns the character count.
 func charsInFile(s string) string {
 	l := len(s)
 	o := strconv.Itoa(l)
 	return o
 }
-
 func main() {
 	var w, l, c bool
 	flag.BoolVar(&w, "w", false, "")
 	flag.BoolVar(&l, "l", false, "")
 	flag.BoolVar(&c, "c", false, "")
 	flag.Parse()
-
 	f := flags{words: w, lines: l, chars: c}
-
 	if flag.NArg() == 0 {
 		file := os.Stdin
 		fi, err := file.Stat()
 		if err != nil {
 			fmt.Println("There was an error retrieving file description:", err)
 		}
-
 		size := fi.Size()
 		if size > 0 {
 			data, err := ioutil.ReadAll(os.Stdin)
 			if err != nil {
 				fmt.Println("There was an error reading from stdin:", err)
 			}
-
 			s := string(data)
 			fmt.Println(f.wcString(s))
 			return
-
 		}
 		fmt.Println("Stdin is empty")
 		return
@@ -97,7 +90,6 @@ func main() {
 		if err != nil {
 			fmt.Println("There was an error reading the file:", err)
 		}
-
 		s := string(data)
 		fmt.Println(f.wcString(s))
 		return
